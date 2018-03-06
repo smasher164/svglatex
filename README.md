96a962ce6e86c20bc812db05bf72a47759937421
Akhil Indurti <aindurti@gmail.com>
2018-03-06 02:12:40 -0500

    Formatting fixes

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # svglatex
 
 svglatex takes a given latex document and produces an SVG image.
+
 Usage:
 ```
 go get -u github.com/smasher164/svglatex
 
 svglatex [-inline] < foo.tex
 ```
-It is a wrapper over the `latex` and `dvisvgm` commands and makes the following assumptions and promises:
+It is a wrapper over `latex` and `dvisvgm` and makes the following assumptions and promises:
 * `latex` is already installed and has an "-output-directory" flag (used to store temporary files).
 * `dvisvgm` is already installed.
 * The latex document is input through Stdin.
--- svglatex.go
+++ svglatex.go
@@ -1,21 +1,25 @@
 // svglatex takes a given latex document and produces an SVG image.
 // Usage:
 //
-// 		svglatex [-inline] < foo.tex
+//     svglatex [-inline] < foo.tex
 //
-// It is a wrapper over the `latex` and `dvisvgm` commands and makes the following assumptions and promises:
-// * latex is already installed and has an "-output-directory" flag (used to store temporary files).
+// It is a wrapper over `latex` and `dvisvgm`and
+// makes the following assumptions and promises:
+// * latex is already installed and has an
+//   "-output-directory" flag (used to store temporary files).
 // * dvisvgm is already installed.
 // * The latex document is input through Stdin.
-// * When successful, it will output the SVG markup to Stdout (everything else is diverted to Stderr).
-
-// The -inline flag causes svglatex to assume the input is an inline equation, and wraps it in the following
-
+// * When successful, it will output the SVG markup to
+//   Stdout (everything else is diverted to Stderr).
+//
+// The -inline flag causes svglatex to assume the input
+// is an inline equation, and wraps it in the following:
+//
 // \documentclass{standalone}
 // \begin{document}
-
+//
 // %THE INPUT GOES HERE%
-
+//
 // \end{document}
 //
 package main
